f61b6f8993345c28c992d0016af2505fe8938369
whitespace-dpo <phamkhanhtn177@gmail.com>
2023-01-05 09:30:34 +0700

    use styled component & css modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,43 @@
+
 import React from 'react';
+import styles from './Button.module.css';
+// import styled from "styled-components";
 
-import './Button.css';
+// const Button = styled.button`
+//   /* Color */
+//   color: white;
+//   background: #008b33;
 
+//   /*Display & box-model */
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #008b33;
+//   border-radius: 5px;
+//   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+//   width: 100%;
+//   /* Text */
+//   font: inherit;
+
+//   /* Other */
+//   cursor: pointer;
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #00742a;
+//     border-color: #00742a;
+//     box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+//   }
+
+//   @media (min-width: 768px){
+//     width: auto;
+//   }
+// `;
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
